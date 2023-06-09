bdbf91237285cc7dbcd05ecbc82eb873ca060579
dell <dell@192.168.1.18>
2023-06-09 15:42:28 +0530

    hi suhani, how are you

--- src/akhiltest/test1.java
+++ src/akhiltest/test1.java
@@ -2,9 +2,13 @@ package akhiltest;
 
 public class test1 {
 
+	// Hi Subhani CECC
+	
+	
 	public static void main(String[] args) {
 		System.out.println("hi");
 
+		System.out.println("hello, there");
 	}
 
 }
